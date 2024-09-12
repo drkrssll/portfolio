1e2d9535fec9079ce2a009709818a92c7f726470
drkrssll <russellderek01@gmail.com>
2024-09-11 23:18:49 -0400

    Added an animation for the button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import './styles.css'
 import React, { useState } from 'react';
 import Image from 'next/image';
 import TypeWriterEffect from './TypeWriterEffect';
@@ -38,14 +39,15 @@ export default function Home() {
           />
           <div className={`transition-opacity duration-500 ${typedText.length === bioText.length ? 'opacity-100' : 'opacity-0'}`}>
             <a
-              className="mt-8 inline-block rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base py-3 px-5 min-w-[176px]"
+              className="mt-8 inline-block rounded-full text-sm sm:text-base py-3 px-5 min-w-[176px] animated-border relative overflow-hidden"
               href="https://github.com/drkrssll"
               target="_blank"
               rel="noopener noreferrer"
             >
-              My Github →
+              <span className="relative z-10">My Github →</span>
             </a>
           </div>
+
         </div>
       </main>
       <footer className="mt-12">
