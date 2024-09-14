4e9754feeb89672602fe72183e503ca955965311
drkrssll <russellderek01@gmail.com>
2024-09-14 19:00:29 -0400

    Updated socials

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       body="These are my socials. For my phone number, email, and other contact methods, please refer to my Resume."
       alignType="justify-between"
       primaryCard={<Card title="Twitter" content="@drkrssll" />}
-      secondaryCard={<Card title="Instagram" content="@piratescanfly" />}
+      secondaryCard={<Card title="Instagram" content="@drkrssll" />}
     />
   );
 }
