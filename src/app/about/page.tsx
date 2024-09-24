00e257f4729f125147da00a6dffc2c739e34dbb1
Derek Russell <russellderek01@gmail.com>
2024-09-23 23:19:51 -0400

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
     <HomePage
       title="Current Projects"
       body={bioText}
-      primaryCard={<Card title="Vizhu" image="/vizhu.png" width={600} height={600} content="Python Library for OSINT investigations. Written using Rust using PyO3." />}
+      primaryCard={<Card title="Vizhu" image="/vizhu.png" width={600} height={600} content="Python Library for OSINT investigations. Written in Rust using PyO3." />}
       secondaryCard={<Card title="Link Hub" image="/link_hub.png" width={400} height={300} content="A link hub for all my socials, simple but effective. Written in TypeScript using Next.js." />}
-      extraCard={<Card title="My Portfolio" content="The source for this site was written using TypeScript using Next.js." />}
+      extraCard={<Card title="My Portfolio" content="The source for this site was written in TypeScript using Next.js." />}
       button="My Github ->"
     />
   );
